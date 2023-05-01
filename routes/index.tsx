195ff4f60b715890b4e2c30fe143bb1ada7bbfaf
Marcu Operario <marcuoperario@gmail.com>
2023-05-01 21:11:52 +0800

    refactor: update routes render condition

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,80 +7,75 @@ import Loading from '../screens/Loading';
 import History from '../screens/History';
 import Diseases from '../screens/Diseases';
 import WhatsNew from '../screens/WhatsNew';
-import { useCachedResources } from '../hooks/useCachedResources';
 import useDarkMode from '../hooks/useDarkMode';
+import useUserID from '../hooks/useUserID';
 
 const Routes = () => {
     const Stack = createNativeStackNavigator<RootStackParamList>();
-    const loading = useCachedResources();
     const { renderTheme } = useDarkMode();
+    const { getID } = useUserID();
+    getID();
     renderTheme();
     
-    if (!loading) {
-        return (
-            <NavigationContainer>
-                <Stack.Navigator
-                    screenOptions = {{
-                        gestureEnabled: false
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions = {{
+                    gestureEnabled: false
+                }}
+            >
+                <Stack.Screen
+                    name = "Home"
+                    component = { Home }
+                    options = {{
+                        headerShown: false,
+                        gestureEnabled: false,
+                        animationTypeForReplace: "pop"
+                    }} 
+                />
+                <Stack.Screen
+                    name = "History"
+                    component = { History }
+                    options = {{
+                        headerShown: false,
+                        gestureEnabled: true
                     }}
-                >
-                    <Stack.Screen
-                        name = "Home"
-                        component = { Home }
-                        options = {{
-                            headerShown: false,
-                            gestureEnabled: false,
-                            animationTypeForReplace: "pop"
-                        }} 
-                    />
-                    <Stack.Screen
-                        name = "History"
-                        component = { History }
-                        options = {{
-                            headerShown: false,
-                            gestureEnabled: true
-                        }}
-                    />
-                    <Stack.Screen
-                        name = "Diseases"
-                        component = { Diseases }
-                        options = {{
-                            headerShown: false,
-                            gestureEnabled: true
-                        }} 
-                    />
-                    <Stack.Screen
-                        name = "WhatsNew"
-                        component = { WhatsNew }
-                        options = {{
-                            headerShown: false,
-                            gestureEnabled: true
-                        }} 
-                    />
-                    <Stack.Screen
-                        name = "Loading"
-                        component = { Loading }
-                        options = {{
-                            headerShown: false,
-                            gestureEnabled: false
-                        }} 
-                    />
-                    <Stack.Screen
-                        name = "Results"
-                        component = { Results }
-                        options = {{
-                            headerShown: false,
-                            gestureEnabled: false
-                        }} 
-                    />
-                </Stack.Navigator>
-            </NavigationContainer>
-        )
-    } else {
-        return (
-            <Loading />
-        )
-    }
+                />
+                <Stack.Screen
+                    name = "Diseases"
+                    component = { Diseases }
+                    options = {{
+                        headerShown: false,
+                        gestureEnabled: true
+                    }} 
+                />
+                <Stack.Screen
+                    name = "WhatsNew"
+                    component = { WhatsNew }
+                    options = {{
+                        headerShown: false,
+                        gestureEnabled: true
+                    }} 
+                />
+                <Stack.Screen
+                    name = "Loading"
+                    component = { Loading }
+                    options = {{
+                        headerShown: false,
+                        gestureEnabled: false
+                    }} 
+                />
+                <Stack.Screen
+                    name = "Results"
+                    component = { Results }
+                    options = {{
+                        headerShown: false,
+                        gestureEnabled: false
+                    }} 
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
 export default Routes;
